c456d2a543d040eb3240bb9e8c94388636ccd97f
D_Njenga <njengadennis.97@gmail.com>
2025-09-17 22:11:16 +0300

    script that prints a message depending of the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+let randomArgs = ["John", "Sam", "Peter", "Lucy"];
+
+if(!randomArgs.length){
+    console.log("No argument");
+}else if(randomArgs.length == 1){
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
+}
