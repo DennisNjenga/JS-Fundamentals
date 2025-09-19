cc38b2169a328ad4376d19f1fc7a0a91de8362cc
D_Njenga <njengadennis.97@gmail.com>
2025-09-19 08:45:08 +0300

    script that prints x times “C is fun

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,10 @@
-const args = process.argv[2];
 
-if(Number(args)){
-    for(let i = 0; i<+ args; i++){
-        console.log("C is fun");
-    }
-}else{
-    console.log(`Missing number of occurrences`);
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
