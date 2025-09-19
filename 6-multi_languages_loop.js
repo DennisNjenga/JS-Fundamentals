2f2401a7847a4279c41d4c7736faf427819795e6
D_Njenga <njengadennis.97@gmail.com>
2025-09-19 06:27:46 +0300

    script that prints 3 lines:

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,3 +2,10 @@ let stringArray = ["C is fun","Python is cool", "JavaScript is amazing"]
 for(let i = 0; i < stringArray.length; i++){
     console.log(stringArray[i]);
 }
+
+let i=0;
+
+while(i < 3){
+    console.log(stringArray[i]);
+    i++;
+}
